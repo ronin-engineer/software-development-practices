92741db7979bce5095e24cdcfd7cb94640ca6e9d
vuong <vuong.lv34@gmail.com>
2023-09-07 23:38:11 +0700

    feat(security): separate controllers

--- src/main/java/dev/ronin_engineer/software_development/application/endpoint/rest/AirportController.java
+++ src/main/java/dev/ronin_engineer/software_development/application/endpoint/rest/AirportController.java
@@ -1,34 +1,25 @@
 package dev.ronin_engineer.software_development.application.endpoint.rest;
 
-
+import dev.ronin_engineer.software_development.application.aop.Authorize;
 import dev.ronin_engineer.software_development.application.dto.response.ResponseDto;
-import dev.ronin_engineer.software_development.application.service.ResponseFactory;
 import dev.ronin_engineer.software_development.domain.airport.entity.Airport;
-import dev.ronin_engineer.software_development.domain.airport.service.AirportService;
+import dev.ronin_engineer.software_development.domain.auth.constant.Action;
+import dev.ronin_engineer.software_development.domain.auth.constant.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping("/${application-name}/v1/airports")
 public class AirportController {
 
-    private final AirportService airportService;
 
-    private final ResponseFactory responseFactory;
-
-    public AirportController(AirportService airportService, ResponseFactory responseFactory) {
-        this.airportService = airportService;
-        this.responseFactory = responseFactory;
-    }
-    
-
-    @GetMapping
-    public ResponseDto getAllAirports() {
-        List<Airport> airports = airportService.getAll();
-        return responseFactory.response(airports);
+    @PostMapping
+    @Authorize(action = Action.CREATE, resource = Resource.AIRPORTS)
+    public String createAirport() {
+        return "OK";
     }
 }
